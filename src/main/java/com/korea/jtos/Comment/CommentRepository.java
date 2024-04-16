fb4752c89d0b60f866eb9673ca1cd631db304573
Dowon Nam <dwnam0114@gmail.com>
2024-04-16 17:40:30 +0900

    test delete

--- src/main/java/com/korea/jtos/Comment/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.korea.jtos.Comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
-
-
-}
